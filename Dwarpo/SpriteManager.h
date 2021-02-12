51c06b78d3f909e5674504879ccb0dfa0482fd3a
Saetch <nils.wriedt@hotmail.com>
2021-02-12 20:03:44 +0100

    added GraphicsResources for SpriteManager

--- Dwarpo/SpriteManager.h
+++ Dwarpo/SpriteManager.h
@@ -6,15 +6,26 @@
 #include <d2d1_1.h> 
 #include <d3d10_1.h>
 #include <wincodec.h>
-
+#include "SpriteWrapper.h"
 #pragma comment(lib, "d3d10_1.lib")
 #pragma comment(lib, "DXGI.lib")
 #pragma comment(lib, "d2d1")
 
 
 #pragma comment(lib, "user32.lib")
+
+template <class T> void SafeRelease(T** ppT);
+
 class SpriteManager
 {
+private:
+    ID2D1Bitmap* staticBuffer;
+    ID2D1BitmapRenderTarget* pstaticBufferTarget;
+    //ID2D1SolidColorBrush** pstaticBufferBrushes;
+
+    ID2D1Bitmap* animationbuffer;
+    ID2D1BitmapRenderTarget* panimationBufferTarget;
+
 public:
 
     IWICImagingFactory* pIWICFactory;
@@ -38,6 +49,18 @@ public:
         UINT destinationHeight,
         ID2D1Bitmap** ppBitmap
     );
+
+
+    SpriteManager() {
+        //TODO allocate Graphics Resources
+    }
+
+    ~SpriteManager() {
+        SafeRelease(&staticBuffer);
+        SafeRelease(&pstaticBufferTarget);
+        SafeRelease(&animationbuffer);
+        SafeRelease(&panimationBufferTarget);
+    }
 
     void setRenderTarget(ID2D1RenderTarget* pMainTarget) {
         pMainRenderTarget = pMainTarget;
